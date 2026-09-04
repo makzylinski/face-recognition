e4cc19156c34afa654e6bf207e08cf1097b8e590
Maks <makzylinski@gmail.com>
2026-09-04 22:59:13 +0200

    added red square on the video image

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
+rectangleImg = np.zeros((512, 512, 3), dtype=np.uint8)
+topLeft = (100, 100)
+topRight = (400, 400)
+colorRed = (0, 0, 255)
 
 while True:
     ret, frame = cap.read()
+
     if not ret:
         break
 
+    cv2.rectangle(frame, topLeft, topRight, colorRed)
     cv2.imshow("Kamera", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
